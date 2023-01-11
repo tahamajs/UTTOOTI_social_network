3680213cc462ca896177b7ac53b40ef04fd694b8
Taha Majlesi <108874557+tahamajs@users.noreply.github.com>
2023-01-11 17:45:32 +0330

    see

--- do.h
+++ do.h
@@ -3,3 +3,5 @@ void doDelete(char *name2, Post *head_post, User *main_user);
 void doInfo(User *main_user);
 void doNewPost(char *name2, char *name_secend, User *main_user, Post *head_post);
 void doFindUser(char *name2, User *head_user);
+
+//hello
